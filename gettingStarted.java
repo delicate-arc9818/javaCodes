ec97e23dabedc2db48df252df84ee61f090e968e
Jitesh Bagri <144794290+delicate-arc9818@users.noreply.github.com>
2024-03-13 04:19:35 +0000

    java day 1

---
+++ gettingStarted.java
@@ -0,0 +1,10 @@
+//class name
+public class gettingStarted{
+
+    // main method
+    public static void main(String args[]){
+        System.out.println("namaste");
+        System.out.println("Hello World");
+    }
+
+}
